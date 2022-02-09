e0aabbf22b01599262e8edc544234b06876726a3
Viktor Viniczei <viniczei.viktor.21f@szlgbp.hu>
2022-02-09 13:53:34 +0100

    Add project files.

---
+++ Git_Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hány db számot szeretnél összeadni?");
+
+            int N = int.Parse(Console.ReadLine());
+
+            int[] szamtomb = new int[N];
+
+            int sum = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                szamtomb[i] = int.Parse(Console.ReadLine());
+                sum += szamtomb[i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
